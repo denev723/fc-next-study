760a28016e95355a734b52b54755a8a5b84bfd12
denev <denev723@gmail.com>
2025-08-07 21:26:53 +0900

    getStaticPaths with fallback

--- src/domains/post/[id]/index.tsx
+++ src/domains/post/[id]/index.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/api/post";
+import { useRouter } from "next/router";
 import { FC } from "react";
 
 interface Props {
@@ -7,6 +8,12 @@ interface Props {
 
 const PostDetail: FC<Props> = (props) => {
   const { post } = props;
+
+  const router = useRouter();
+  if (router.isFallback) {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <h1>{post.title}</h1>
